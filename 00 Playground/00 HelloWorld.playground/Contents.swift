f7d8c4f5ac9e32ebe59c266abb5ca2a0194d641c
SushantP199 <sushantpagam199@gmail.com>
2022-09-03 15:37:11 +0530

    Till 01 Variables And Constants Completed

---
+++ 00 Playground/00 HelloWorld.playground/Contents.swift
@@ -0,0 +1,16 @@
+var greeting = "Hello, Sushant"
+print(greeting)
+
+/*
+    Writinhg hello world is one of the easiest of the program but whole
+    point to writing this we can verify that we have workable environment
+    where we can write some code. It also give basic overview about the
+    language that how syntax looks like and we can move ahead in that.
+  
+ 
+    As while writing & compiling this program I got couple errors related
+    to configurations and I fixed that configuaration successfully. And
+    now able to write and execute this program properly, which indicates
+    yes I have now workable environment and I can proceed my futher
+    development journey without any hesitation.
+*/
---
+++ 00 Playground/01 Variables And Constants.playground/Contents.swift
@@ -0,0 +1,33 @@
+// variable declaration
+var username = "Sushant"
+
+// constant declaration
+let accountId = 3681000110
+
+username = "Sushant Pagam" // variable var value can be changed
+
+/*
+    accountId = 3681000450
+    
+    above line of code will give error as accountId is constant
+    once initialized its value cannot be changed futher
+*/
+
+/*
+    var numberOfLoginsInDay = 34
+    to add more precision and to accept strict type of data in above variable
+*/
+var numberOfLoginsInDay: Int = 34
+/*
+    Or first declare with must type then initialize or assign
+ 
+    var numberOfLoginsInDay : Int
+    numberOfLoginsInDay = 34
+*/
+
+// one more common syntax to declare multiple varibles of with must same strict type in one line
+var name, email, address : String
+
+/*
+    We will try to use more constants than variables as this is a good practice in programming
+*/
