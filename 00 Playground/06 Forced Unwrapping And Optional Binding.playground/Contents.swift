18a52dc650ba95d4269c47dfb4a30bd44eddd770
SushantP199 <sushantpagam199@gmail.com>
2022-09-07 14:35:26 +0530

    06 Forced Unwrapping And Optional Binding Completed

---
+++ 00 Playground/06 Forced Unwrapping And Optional Binding.playground/Contents.swift
@@ -0,0 +1,15 @@
+// Optionals
+
+var userCity : String?
+var accountActive: Bool?
+
+userCity = "Delhi"
+print(userCity)
+
+// Forced Unwrapping
+
+print(userCity!) // while using forced unwrapping for handling optional type value if value is nil then program gets crashed
+
+// Optional Binding
+
+// proper way of handling optional type values that is to be learned later on
