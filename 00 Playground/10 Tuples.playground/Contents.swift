529db7c4257ce60a6259f1b42ea1cbc41ba43b68
SushantP199 <sushantpagam199@gmail.com>
2022-09-09 19:50:27 +0530

    10 Tuples Updated

--- 00 Playground/10 Tuples.playground/Contents.swift
+++ 00 Playground/10 Tuples.playground/Contents.swift
@@ -8,11 +8,19 @@ var course : (String, Int, Bool) = ("iOS App Dev Course", 999, true)
 // better version of declaring tuple
 var courze : (courzeName : String, courzePrice : Int, courzeStatus : Bool) = ("iOS App Dev Course", 999, true)
 
+// direct assignment of tuple without strict type mention
+var covrse = ("iOS App Dev Course", 999, true)
+
+// better version of direct assignemnt of tuple without strict type mention
+var covrze = (covrzeName : "iOS App Dev Course", covrzePrice : 999, covrzeStatus : true)
+
 // Accessing tuple elements
 course.0
 courze.courzeName
+covrse.0
+covrze.covrzeName
 
-var (courseName, coursePrice, courseStatus) = courze
+var (courseName, coursePrice, courseStatus) = covrze
 
 courseName
 coursePrice
